08abd22ac287b33ab29307dfa4cc5d37d682444e
nathanoy <nathanoy@gmx.de>
2023-11-14 23:15:44 +0100

    minor polish

--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,179 @@
-use image::imageops::{resize, FilterType};
 use image::io::Reader as ImageReader;
-use image::{DynamicImage, Rgb};
+use image::{DynamicImage, Rgb, RgbImage};
 use std::error::Error;
-use std::iter::Sum;
-use std::ops::{Add, Div};
-use std::process::Command;
-
 use std::time::Instant;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let t0 = Instant::now();
-    let mut img = ImageReader::open("img.jpg")?.decode()?.to_rgb8();
-    println!("Read in: {:.2}s", t0.elapsed().as_secs_f32());
-    let t0 = Instant::now();
-    let mut img = resize(&img, 800, 600, FilterType::Triangle);
-    println!("Resized in: {:.2}s", t0.elapsed().as_secs_f32());
+mod util;
+use crate::util::{average, color_distance, show_img, Point};
 
-    let t0 = Instant::now();
+const IS_PART_COLOR: Rgb<u8> = Rgb([255, 255, 255]);
+const CROSS_COLOR: Rgb<u8> = Rgb([255, 255, 255]);
+const IS_BACKGROUND_SCANNED: Rgb<u8> = Rgb([255, 0, 0]);
+const BACKGROUND: Rgb<u8> = Rgb([0, 0, 0]);
 
-    let p0 = *img.pixels().next().unwrap();
+fn fill_adjacent(img: &mut RgbImage, x0: u32, y0: u32) -> u32 {
+    let mut nodes = vec![];
+    let px = *img.get_pixel_mut(x0, y0);
+    if px == BACKGROUND {
+        nodes.push((x0, y0))
+    }
 
-    const BACKGROUND_COL: Rgb<u8> = Rgb([40, 40, 50]);
-    // Color key
-    for px in img.pixels_mut() {
-        let pi = *px;
-        let d = distance(p0, pi);
-        if d < 25.0 {
-            *px = BACKGROUND_COL;
+    /// checked minus one
+    fn cm(a: u32) -> u32 {
+        if a == 0 {
+            0
+        } else {
+            a - 1
         }
     }
 
-    let need_a_name: Vec<_> = img
+    let mut count = 0;
+    while let Some((xi, yi)) = nodes.pop() {
+        for y in cm(yi)..=(yi + 1) {
+            for x in cm(xi)..=(xi + 1) {
+                if let Some(px) = img.get_pixel_mut_checked(x, y) {
+                    if *px == BACKGROUND {
+                        *px = IS_BACKGROUND_SCANNED;
+                        nodes.push((x, y));
+                        count += 1;
+                    }
+                }
+            }
+        }
+    }
+    count
+}
+
+fn add_adjacent_to_shape(img: &mut RgbImage, x0: u32, y0: u32) {
+    let mut nodes = vec![];
+    let px = *img.get_pixel_mut(x0, y0);
+    if px == IS_BACKGROUND_SCANNED {
+        nodes.push((x0, y0))
+    }
+
+    /// checked minus one
+    fn cm(a: u32) -> u32 {
+        if a == 0 {
+            0
+        } else {
+            a - 1
+        }
+    }
+
+    while let Some((xi, yi)) = nodes.pop() {
+        for y in cm(yi)..=(yi + 1) {
+            for x in cm(xi)..=(xi + 1) {
+                if let Some(px) = img.get_pixel_mut_checked(x, y) {
+                    if *px == IS_BACKGROUND_SCANNED {
+                        *px = IS_PART_COLOR;
+                        nodes.push((x, y));
+                    }
+                }
+            }
+        }
+    }
+}
+
+/// Finds the Center of Gravity of a differently colored object in an Image.
+/// returns None when no object is detected
+fn find_center_of_gravity(img: &mut RgbImage) -> Option<Point<u32>> {
+    // TODO: user selectable pixel, currently just the first one
+    let p0 = *img
+        .pixels()
+        .next()
+        .expect("at least one pixel in the src image");
+
+    // To keep track of what pixels belong to the Object. All pixels with the color IS_PART_COLOR are part
+    let mut piece_map = RgbImage::new(img.width(), img.height());
+    piece_map.pixels_mut().for_each(|x| *x = IS_PART_COLOR);
+
+    // Color key
+    for (x, y, px) in img.enumerate_pixels_mut() {
+        if color_distance(p0, *px) < 35.0 {
+            // *px = BACKGROUND_COL;
+            *piece_map.get_pixel_mut(x, y) = BACKGROUND;
+        }
+    }
+
+    let provisional_shape_pixel_count = piece_map.pixels().filter(|&&x| x != BACKGROUND).count();
+
+    // Fix small holes
+    const MIN_HOLE_PERCENT_SIZE: f32 = 0.01;
+    for y in 0..piece_map.height() {
+        for x in 0..piece_map.width() {
+            if *piece_map.get_pixel(x, y) == BACKGROUND {
+                let filled = fill_adjacent(&mut piece_map, x, y);
+                let p = filled as f32 / provisional_shape_pixel_count as f32;
+                if p < MIN_HOLE_PERCENT_SIZE {
+                    add_adjacent_to_shape(&mut piece_map, x, y);
+                }
+            }
+        }
+    }
+
+    // Darken the background
+    piece_map.enumerate_pixels().for_each(|(x, y, px)| {
+        if *px != IS_PART_COLOR {
+            let px = img.get_pixel_mut(x, y);
+            let Rgb([r, g, b]) = *px;
+            const DD: u8 = 3; // Darkness divident
+            *px = Rgb([r / DD, g / DD, b / DD]);
+        }
+    });
+
+    // Collect all points of the shape, in order to average them
+    let pixels_of_shape: Vec<_> = piece_map
         .enumerate_pixels()
         .filter_map(|(x, y, px)| {
-            if *px != BACKGROUND_COL {
+            if *px == IS_PART_COLOR {
                 Some(Point(x, y))
             } else {
                 None
             }
         })
         .collect();
-    let Point(cogx, cogy) = average(&need_a_name[..]).unwrap();
+
+    average(&pixels_of_shape[..])
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let filename = std::env::args()
+        .nth(1)
+        .expect("exactly one command line argument: the input path");
+
+    let mut t0 = Instant::now();
+    let mut img = ImageReader::open(filename)?.decode()?.to_rgb8();
+    println!("Read Image in: {:.2}s", t0.elapsed().as_secs_f32());
+
+    // t0 = Instant::now();
+    // let mut img = resize(&img, 800, 600, FilterType::Triangle);
+    // println!("Resized in: {:.2}s", t0.elapsed().as_secs_f32());
+
+    // Start
+    t0 = Instant::now();
+
+    let Point(cogx, cogy) =
+        find_center_of_gravity(&mut img).ok_or("No Shape detected".to_owned())?;
 
     // Draw y
     for x in 0..img.width() {
-        let px = img.get_pixel_mut(x, cogy);
-        *px = Rgb([255, 0, 0]);
+        *img.get_pixel_mut(x, cogy + 1) = CROSS_COLOR;
+        *img.get_pixel_mut(x, cogy) = CROSS_COLOR;
+        *img.get_pixel_mut(x, cogy - 1) = CROSS_COLOR;
     }
 
     // Draw x
     for y in 0..img.height() {
-        let px = img.get_pixel_mut(cogx, y);
-        *px = Rgb([255, 0, 0]);
+        *img.get_pixel_mut(cogx + 1, y) = CROSS_COLOR;
+        *img.get_pixel_mut(cogx, y) = CROSS_COLOR;
+        *img.get_pixel_mut(cogx - 1, y) = CROSS_COLOR;
     }
 
+    println!("Proccessed in: {:.2}s", t0.elapsed().as_secs_f32());
+
+    t0 = Instant::now();
     show_img(&DynamicImage::ImageRgb8(img))?;
-
-    println!("Work in: {:.2}s", t0.elapsed().as_secs_f32());
+    println!("Saved and displayed in: {:.2}s", t0.elapsed().as_secs_f32());
 
     Ok(())
-}
-
-#[derive(Debug)]
-struct Point<T>(T, T);
-
-impl<'a, T> Sum<&'a Self> for Point<T>
-where
-    T: Add<Output = T> + Default + Copy,
-{
-    fn sum<I>(iter: I) -> Self
-    where
-        I: Iterator<Item = &'a Self>,
-    {
-        iter.fold(Self(Default::default(), Default::default()), |acc, b| {
-            Self(acc.0 + b.0, acc.1 + b.1)
-        })
-    }
-}
-
-impl<T> Div<T> for Point<T>
-where
-    T: Div<Output = T> + Copy,
-{
-    type Output = Self;
-    fn div(self, divisor: T) -> Self {
-        Self(self.0 / divisor, self.1 / divisor)
-    }
-}
-
-fn show_img(img: &DynamicImage) -> Result<(), Box<dyn Error>> {
-    const PATH: &str = "tmp.png";
-    img.save(PATH)?;
-    Command::new("cmd").args(["/C", "start", PATH]).spawn()?;
-    Ok(())
-}
-
-fn distance(c1: Rgb<u8>, c2: Rgb<u8>) -> f32 {
-    // let Rgb([r0, g0, b0]) = c1;
-    // let Rgb([r, g, b]) = c2;
-
-    // // Pytagoras3d
-    // (((r0 as i32 - r as i32) * (r0 as i32 - r as i32)
-    //     + (g0 as i32 - g as i32) * (g0 as i32 - g as i32)
-    //     + (b0 as i32 - b as i32) * (b0 as i32 - b as i32)) as f32)
-    //     .sqrt()
-    let Rgb(c1) = c1;
-    let Rgb(c2) = c2;
-    delta_e::DE2000::from_rgb(&c1, &c2)
-}
-
-/// Fails when itms is empty or when the length of itms overflows W!
-fn average<'a, T, W>(itms: &'a [T]) -> Option<T>
-where
-    T: Sum<&'a T> + Div<W, Output = T>,
-    W: TryFrom<usize>,
-{
-    if itms.is_empty() {
-        return None;
-    }
-    Some(itms.iter().sum::<T>() / itms.len().try_into().ok()?)
 }
---
+++ src/util.rs
@@ -0,0 +1,66 @@
+use std::error::Error;
+use std::iter::Sum;
+use std::ops::{Add, Div};
+use std::process::Command;
+
+use image::{DynamicImage, Rgb};
+
+#[derive(Debug)]
+pub struct Point<T>(pub T, pub T);
+
+impl<'a, T> Sum<&'a Self> for Point<T>
+where
+    T: Add<Output = T> + Default + Copy,
+{
+    fn sum<I>(iter: I) -> Self
+    where
+        I: Iterator<Item = &'a Self>,
+    {
+        iter.fold(Self(Default::default(), Default::default()), |acc, b| {
+            Self(acc.0 + b.0, acc.1 + b.1)
+        })
+    }
+}
+
+impl<T> Div<T> for Point<T>
+where
+    T: Div<Output = T> + Copy,
+{
+    type Output = Self;
+    fn div(self, divisor: T) -> Self {
+        Self(self.0 / divisor, self.1 / divisor)
+    }
+}
+
+/// Fails when itms is empty or when the length of itms overflows W!
+pub fn average<'a, T, W>(itms: &'a [T]) -> Option<T>
+where
+    T: Sum<&'a T> + Div<W, Output = T>,
+    W: TryFrom<usize>,
+{
+    if itms.is_empty() {
+        return None;
+    }
+    Some(itms.iter().sum::<T>() / itms.len().try_into().ok()?)
+}
+
+pub fn show_img(img: &DynamicImage) -> Result<(), Box<dyn Error>> {
+    const PATH: &str = "tmp.jpg";
+    img.save(PATH)?;
+    Command::new("cmd").args(["/C", "start", PATH]).spawn()?;
+    Ok(())
+}
+
+pub fn color_distance(Rgb(c1): Rgb<u8>, Rgb(c2): Rgb<u8>) -> f32 {
+    // Alternative color distance function
+    // let Rgb([r0, g0, b0]) = c1;
+    // let Rgb([r, g, b]) = c2;
+
+    // // Pytagoras3d
+    // (((r0 as i32 - r as i32) * (r0 as i32 - r as i32)
+    //     + (g0 as i32 - g as i32) * (g0 as i32 - g as i32)
+    //     + (b0 as i32 - b as i32) * (b0 as i32 - b as i32)) as f32)
+    //     .sqrt()
+
+    delta_e::DE2000::from_rgb(&c1, &c2)
+}
